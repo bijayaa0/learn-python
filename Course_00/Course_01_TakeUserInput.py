e9b85b7af056146408a9b4a7c2d77b025ef6b79e
bijayaa0 <bijayapun299@gmail.com>
2026-01-13 01:50:54 +0545

    Add user input functionality for name, age, and faculty

---
+++ Course_00/Course_01_TakeUserInput.py
@@ -0,0 +1,7 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+faculty = input("Enter your faculty: ")
+
+print("Hello ",name)
+print("You are",age,"years old")
+print("Welcome to",faculty)
